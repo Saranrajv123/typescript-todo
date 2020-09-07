55afc50e80f117dbff9102d46f19ec11a5415ae6
saranrajv123 <vsaranraj16@gmail.com>
2020-09-07 20:25:52 +0530

    complete typescript todo's

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type fromElem = React.FormEvent<HTMLFormElement>;
+interface ITodo {
+  text: string;
+  complete: boolean;
 }
+
+const App = () => {
+  const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handleSubmit = (event: fromElem): void => {
+    event.preventDefault();
+    addTodo(value);
+    setValue("");
+  };
+
+  const addTodo = (text: string): void => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+  };
+
+  return (
+    <Fragment>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          required
+        />
+        <button> Add Todo </button>
+      </form>
+
+      <section>
+        {todos.map((item: ITodo, index: number) => {
+          return (
+            <Fragment key={index}>
+              <div>{item.text}</div>
+              <button onClick={() => completeTodo(index)}>
+                {item.complete ? "Incomplete" : "complete"}
+              </button>
+            </Fragment>
+          );
+        })}
+      </section>
+    </Fragment>
+  );
+};
 
 export default App;
